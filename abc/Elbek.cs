b02f216448758e2ffa1ce32124d14ad733c847c7
bekelbek2 <90567318+bekelbek2@users.noreply.github.com>
2021-12-12 19:11:06 +0500

    Add files via upload

---
+++ abc/Elbek.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace abc
+{
+    class Elbek
+    {
+        private int year = 13;
+
+        //My year cannot be changed so set is private to other classes
+        //But you can see my year))
+        public int Year
+        {
+            get
+            {
+                return year;
+            }
+            private set
+            {
+                Year = value;
+            }
+        }
+
+        private string fullname = "Elbek Rakhimjonov Rustamovich";
+
+        //My full name cannot be changed so set is private to other classes
+        //But you can see my full name too))
+        public string fullName
+        {
+            get
+            {
+                return fullname;
+            }
+            private set
+            {
+                fullName = value;
+            }
+        }
+        
+        //ShowInfo funciton shows my Full name and year
+        public void ShowInfo()
+        {
+            Console.WriteLine($"Full name: {fullName}");
+            Console.WriteLine($"Year: {Year}");
+            Console.WriteLine();
+        }
+        
+        //...
+        public void AddYear(int add)
+        {
+            Year += add;
+        }
+    }
+}
---
+++ abc/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace abc
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            //Calling person Elbek
+            Elbek elbek = new Elbek();
+
+            //Showing Elbek's info
+            elbek.ShowInfo();
+            
+            //creating string that shows day
+            string Today = DateTime.Now.ToString("dd'/'MM'/'yyyy");
+
+            //You relaised why it's...
+            if (Today == "05/01/2022")
+            {
+                elbek.AddYear(1);
+
+                //Let's see what've been changed
+                elbek.ShowInfo();
+            }
+        }
+    }
+}
